eaf0697a746ee06e26595e7d6327b7c6a79245f4
Andualem <andualem.fereja12@gmail.com>
2024-12-07 16:52:02 +0300

    editing feature added

--- app/500.js
+++ app/500.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 export default function Error({ error, reset }) {
     return (
-        <div className="text-center mt-[24vh]">
+        <div className="text-center max-h-[100vh] mt-[24vh]">
             <h2 className="text-[100px] text-zinc-500 font-medium">500</h2>
             <h3 className="text-2xl text-gray-700 font-medium">Internal server error</h3>
             <div className="pt-5 mx-4 text-sm font-bold text-zinc-700">
-                Back to <Link className="text-sky-700 text-lg bg-zinc-100 hover:bg-zinc-300 rounded-md p-1" href="/">Home</Link>
+                Back to <Link className="text-sky-700 text-lg bg-zinc-100 hover:bg-zinc-200 rounded-md p-1" href="/">Home</Link>
             </div>
         </div>
     )
